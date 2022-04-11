6dd203224a05fa67a77bb8d3ca634b56969efac7
Phenya76 <103383311+Phenya76@users.noreply.github.com>
2022-04-11 10:20:53 +0300

    Solution of second task

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace FirstMeeting
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            char symbol = 'a';
+            string myName = "Aleks";
+            int cold = -20;
+            uint helth = 100;
+            float sumRur = 12.5f;
+            double price = 4990;
+            decimal balance = 300000.87984m;
+            ushort amountPatrons = 60;
+            short houseStage = -1;
+            sbyte mineStage = -127;
+            byte amountMinerals = 255;
+        }
+    }
+}
