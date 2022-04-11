6b6a032d80776745b066b10a97378075e74af43e
Phenya <Phenya@DESKTOP-SFU7U43>
2022-04-11 10:16:08 +0300

    second

--- Program.cs
+++ Program.cs
@@ -4,8 +4,17 @@
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("ЯЮниор");
+            char symbol = 'a';
+            string myName = "Aleks";
+            int cold = -20;
+            uint helth = 100;
+            float sumRur = 12.5f;
+            double price = 4990;
+            decimal balance = 300000.87984m;
+            ushort amountPatrons = 60;
+            short houseStage = -1;
+            sbyte mineStage = -127;
+            byte amountMinerals = 255;
         }
-
     }
 }
